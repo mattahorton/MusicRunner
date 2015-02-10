023a831e334a81d95f06d67b546df3affea2d6d9
mattahorton <matthew.a.horton@gmail.com>
2015-02-09 16:29:33 -0800

    Bass workssssssssss

--- MusicRunner/core/MHCore.mm
+++ MusicRunner/core/MHCore.mm
@@ -13,6 +13,8 @@
 #import <vector>
 #import <AVFoundation/AVFoundation.h>
 #import "AEBlockChannel.h"
+#import "bassmidi.h"
+#import "bass.h"
 
 #define SRATE 24000
 #define FRAMESIZE 512
@@ -25,6 +27,9 @@ UInt32 g_numFrames;
 
 // buffer
 float * g_buffer = NULL;
+
+static HSTREAM stream;
+static BASS_MIDI_FONT fonts[2];
 
 
 @implementation MHCore {
@@ -103,10 +108,14 @@ float * g_buffer = NULL;
     
     //LOAD MIDI
     
-    NSLog(@"%@", filePath);
+//    NSLog(@"%@", filePath);
     
     scoreReader.load([filePath UTF8String]);
     double bpm = scoreReader.getBPM();
+    
+    
+    //START BASS
+    bassInit();
     
     
 
@@ -120,13 +129,108 @@ float * g_buffer = NULL;
 }
 
 void nextMidiNote (/*YScoreReader reader*/){
-    NSLog(@"Note");
-    NSLog(@"%f",[MHCore sharedInstance]->scoreReader.getBPM());
+//    NSLog(@"Note");
+//    NSLog(@"%f",[MHCore sharedInstance]->scoreReader.getBPM());
     [MHCore sharedInstance]->scoreReader.next(7);
     const NoteEvent *note = [MHCore sharedInstance]->scoreReader.current(7);
 //    NSLog(@"%d",note->data2);
     
     delete note;
+}
+
+void bassInit(){
+    
+    int err;
+    
+    
+    
+    // check the correct BASS was loaded
+    
+    if (HIWORD(BASS_GetVersion())!=BASSVERSION) {
+        
+        NSLog(@"An incorrect version of BASS was loaded");
+        
+    }
+    
+    
+    
+    // initialize default output device
+    
+    if (!BASS_Init(-1,SRATE,0,0,NULL))
+        
+        NSLog(@"Can't initialize output device");
+    
+    
+    
+    //might not need 16 input channels but it also might not hurt anything
+    
+    stream=BASS_MIDI_StreamCreate(NUM_CHANNELS,0,1); // create the MIDI stream (16 MIDI channels for device input + 1 for keyboard input)
+    
+    
+    
+    BASS_ChannelSetAttribute(stream,BASS_ATTRIB_NOBUFFER,1); // no buffering for minimum latency
+    
+    
+    
+    for(int i = 0; i < NUM_CHANNELS; i++){
+        
+        BASS_ChannelPlay(stream,0); // start it
+        
+        err = BASS_ErrorGetCode();
+        
+        if (err) NSLog(@"bass error code %d after initializing channel %d", err, i);
+        
+    }
+    
+    
+    
+    HSOUNDFONT sfont1=BASS_MIDI_FontInit([[[NSBundle mainBundle] pathForResource: @"Gort" ofType: @"SF2"] UTF8String], 0);
+    
+    //HSOUNDFONT sfont2=BASS_MIDI_FontInit([[[NSBundle mainBundle] pathForResource: @"RolandChurchBells" ofType: @"sf2"] UTF8String], 0);
+    
+    //HSOUNDFONT sfont3=BASS_MIDI_FontInit([[[NSBundle mainBundle] pathForResource: @"rocking8m11e" ofType: @"sf2"] UTF8String], 0);
+    
+    //HSOUNDFONT sfont4=BASS_MIDI_FontInit([[[NSBundle mainBundle] pathForResource: @"SoloSynth" ofType: @"SF2"] UTF8String], 0);
+    
+    
+    
+    
+    
+    err = BASS_ErrorGetCode();
+    
+    NSLog(@"bass error code %d", err);
+    
+    
+    
+    //HSOUNDFONT sfont2=BASS_MIDI_FontInit("/Volumes/Bitchin\'ASSD/Andrew/Desktop/256/b/GLoiler/GLoiler/data/RolandChurchBells.sf2", 0);
+    
+    //err = BASS_ErrorGetCode();
+    
+    //NSLog(@"bass error code %d", err);
+    
+    
+    
+    
+    fonts[0].font = sfont1;
+    
+    fonts[0].preset = -1;
+    
+    fonts[0].bank = 0;
+    
+    
+    
+    //fonts[1].font = sfont2;
+    
+    //fonts[1].preset = 10;
+    
+    //fonts[1].bank = 0;
+    
+    
+    
+    BASS_MIDI_StreamSetFonts(stream, fonts, 1);
+    
+    BASS_MIDI_StreamEvent(stream, 0,MIDI_EVENT_NOTE,MAKEWORD(64,100));
+    
 }
 
 @end
