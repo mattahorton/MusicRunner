0a51b32b89be88b193496426dae61bb8226f54c3
mattahorton <matthew.a.horton@gmail.com>
2015-02-10 00:53:45 -0800

    Monitoring MIDI events

--- MusicRunner/core/MHCore.mm
+++ MusicRunner/core/MHCore.mm
@@ -96,33 +96,11 @@ float sampRate;
                                                   UInt32 frames,
                                                   AudioBufferList *audio) {
         
-        int sampCount = [[MHMediator sharedInstance] getCurrentSamp];
-        
         for ( int i=0; i<frames; i++ ) {
             ((float*)audio->mBuffers[0].mData)[i] = ((float*)audio->mBuffers[1].mData)[i] = 0;
-            
-            sampCount++;
-            
-            [[MHMediator sharedInstance] updateCountWithSampleCount: sampCount];
         }
         
     }];
-    
-    
-    
-    
-    //LOAD MIDI
-    
-//    NSLog(@"%@", filePath);
-    
-    scoreReader.load([filePath UTF8String]);
-    double bpm = scoreReader.getBPM();
-    
-
-    // REGISTER BEAT CALLBACK
-    
-//    [self.mediator registerCallbackWithPeriod:(framesize*60/(bpm)) andCallback:&nextMidiNote];
-//    [self.mediator registerCallbackWithCount:1 andCallback:&nextMidiNote andArg:4];
     
     
     // START AUDIO
@@ -182,8 +160,10 @@ void bassInit(){
     
     //might not need 16 input channels but it also might not hurt anything
     
-//    stream=BASS_MIDI_StreamCreate(NUM_CHANNELS,0,1); // create the MIDI stream (16 MIDI channels for device input + 1 for keyboard input)
     stream = BASS_MIDI_StreamCreateFile(false, [[[NSBundle mainBundle] pathForResource:@"watchtower" ofType:@"mid"] UTF8String], 0, 0, BASS_STREAM_AUTOFREE, 0);
+    
+    // set up midi sync to get midi events
+    BASS_ChannelSetSync(stream, BASS_SYNC_MIDI_EVENT|BASS_SYNC_MIXTIME, MIDI_EVENT_NOTE, NoteProc, 0);
     
     
     
@@ -205,27 +185,11 @@ void bassInit(){
     
     HSOUNDFONT sfont1=BASS_MIDI_FontInit([[[NSBundle mainBundle] pathForResource: @"Gort" ofType: @"SF2"] UTF8String], 0);
     
-    //HSOUNDFONT sfont2=BASS_MIDI_FontInit([[[NSBundle mainBundle] pathForResource: @"RolandChurchBells" ofType: @"sf2"] UTF8String], 0);
-    
-    //HSOUNDFONT sfont3=BASS_MIDI_FontInit([[[NSBundle mainBundle] pathForResource: @"rocking8m11e" ofType: @"sf2"] UTF8String], 0);
-    
-    //HSOUNDFONT sfont4=BASS_MIDI_FontInit([[[NSBundle mainBundle] pathForResource: @"SoloSynth" ofType: @"SF2"] UTF8String], 0);
-    
-    
-    
     
     
     err = BASS_ErrorGetCode();
     
-    NSLog(@"bass error code %d", err);
-    
-    
-    
-    //HSOUNDFONT sfont2=BASS_MIDI_FontInit("/Volumes/Bitchin\'ASSD/Andrew/Desktop/256/b/GLoiler/GLoiler/data/RolandChurchBells.sf2", 0);
-    
-    //err = BASS_ErrorGetCode();
-    
-    //NSLog(@"bass error code %d", err);
+    if (err) NSLog(@"bass error code %d", err);
     
     
     
@@ -238,16 +202,13 @@ void bassInit(){
     
     
     
-    //fonts[1].font = sfont2;
-    
-    //fonts[1].preset = 10;
-    
-    //fonts[1].bank = 0;
-    
-    
-    
     BASS_MIDI_StreamSetFonts(stream, fonts, 1);
     
+}
+
+void CALLBACK NoteProc(HSYNC handle, DWORD channel, DWORD data, void *user)
+{
+    return;
 }
 
 @end
